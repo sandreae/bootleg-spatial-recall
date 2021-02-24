a186d01711ee2e995ffe0ab95da54c1bc6499bf9
sandreae <git@contact.samandreae.com>
2021-02-24 16:13:38 +0000

    Remove unhandled error catcher from server index

--- api/index.js
+++ api/index.js
@@ -1,11 +1,5 @@
 const dotenv = require('dotenv');
 
-process.on('uncaughtException', (err) => {
-  console.log('UNCAUGHT EXCEPTION! 💥 Shutting down...');
-  console.log(err.name, err.message);
-  process.exit(1);
-});
-
 dotenv.config();
 const app = require('./app');
 
