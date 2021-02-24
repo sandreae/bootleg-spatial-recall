f81a74834a216f4a6d56fa1ce5451c15be4af920
sandreae <git@contact.samandreae.com>
2021-02-24 22:05:30 +0000

    Refactor server index and catch unhandled errors

--- api/index.js
+++ api/index.js
@@ -1,18 +1,53 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
-dotenv.config();
+process.on('uncaughtException', (err) => {
+  console.log('UNCAUGHT EXCEPTION! 💥 Shutting down...');
+  console.log(err.name, err.message);
+  process.exit(1);
+});
+
+dotenv.config({ path: '../.env' });
 const app = require('./app');
 
-require('./database/db');
+const mongoURI = process.env.MONGO_URL;
+const options = {
+  keepAlive: true,
+  keepAliveInitialDelay: 300000,
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+};
+
+mongoose
+  .connect(mongoURI, options)
+  .then(() => console.log('DB connection successful!'));
 
 // Export express app
-module.exports = { path: '/api', handler: app };
+module.exports = { prefix: false, handler: app };
 
 // Start standalone server if directly running
+let server;
+
 if (require.main === module) {
   const port = process.env.PORT || 3001;
-  app.listen(port, () => {
+  server = app.listen(port, () => {
     // eslint-disable-next-line no-console
     console.log(`API server listening on port ${port}`);
   });
 }
+
+process.on('unhandledRejection', (err) => {
+  console.log('UNHANDLED REJECTION! 💥 Shutting down...');
+  console.log(err.name, err.message);
+  server.close(() => {
+    process.exit(1);
+  });
+});
+
+process.on('SIGTERM', () => {
+  console.log('👋 SIGTERM RECEIVED. Shutting down gracefully');
+  server.close(() => {
+    console.log('💥 Process terminated!');
+  });
+});
