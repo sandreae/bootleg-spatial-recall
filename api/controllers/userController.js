f3e5674d8952ab29e6fc34fb73ff88f60381d240
sandreae <git@contact.samandreae.com>
2021-02-28 17:22:06 +0000

    Return 404 if user not found

--- api/controllers/userController.js
+++ api/controllers/userController.js
@@ -40,6 +40,10 @@ exports.updateUser = catchAsync(async (req, res, next) => {
     },
   );
 
+  if (!updatedUser) {
+    return next(new AppError('No user found with that ID', 404));
+  }
+
   res.status(200).json({
     status: 'success',
     data: {
@@ -58,7 +62,11 @@ exports.deleteUser = catchAsync(async (req, res, next) => {
 });
 
 exports.getUser = catchAsync(async (req, res, next) => {
-  const user = await User.findById(req.user.id);
+  const user = await User.findById(req.params.id);
+
+  if (!user) {
+    return next(new AppError('No user found with that ID', 404));
+  }
 
   res.status(200).json({
     status: 'success',
