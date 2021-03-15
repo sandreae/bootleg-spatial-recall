f43c1aabe078e9ffc1644a92cea425a971496fb9
sandreae <git@contact.samandreae.com>
2021-03-15 12:04:08 +0000

    Style default layout based on router class middleware

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,9 @@
 <template>
   <div>
-    <div class="container--flex-column-centre">
+    <div
+      class="container--flex-column-centre"
+      :class="[`body-${$store.state.class.bodyClass}`]"
+    >
       <Nav />
       <Nuxt keep-alive />
     </div>
@@ -8,7 +11,20 @@
 </template>
 
 <style scoped>
+.body-index {
+  background-color: var(--main-bg-color) !important;
+}
+
+.body-about {
+  background-color: var(--about-page-bg-color) !important;
+}
+
+.body-impulses-upload {
+  background-color: var(--upload-page-bg-color) !important;
+}
+
 .container--flex-column-centre {
+  background-color: var(--main-bg-color);
   width: 100%;
   min-height: 100vh;
   justify-content: flex-start;
